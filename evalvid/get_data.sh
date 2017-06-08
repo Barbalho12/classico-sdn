2bfecffcdb416b396a1301fc1cd21d079c9d3afe
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-06-08 08:01:19 -0300

    update scripts

---
+++ evalvid/get_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+H3_USER='uplab'
+H3='192.168.2.115'
+H3_DIRECTORY='workspace/classico-sdn/evalvid/files/*'
+
+H1_USER='uplab'
+H1='192.168.2.120'
+H1_DIRECTORY='workspace/classico-sdn/evalvid/files/*'
+
+
+LOCAL_USER='barbalho'
+LOCAL_DIRECTORY='/home/barbalho/workspace/classico-sdn/evalvid/files'
+
+
+#scp $H3_USER@$H3:$H3_DIRECTORY $LOCAL_DIRECTORY
+
+
+
+scp $H1_USER@$H1:$H1_DIRECTORY $LOCAL_DIRECTORY
