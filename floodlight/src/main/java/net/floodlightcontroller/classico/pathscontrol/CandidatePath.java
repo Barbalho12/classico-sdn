9eeed80bb2c59a59be352c91200b73568b03a795
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-07-21 23:00:20 -0300

    Adding GrupMod

--- floodlight/src/main/java/net/floodlightcontroller/classico/pathscontrol/CandidatePath.java
+++ floodlight/src/main/java/net/floodlightcontroller/classico/pathscontrol/CandidatePath.java
@@ -1,9 +1,11 @@
 package net.floodlightcontroller.classico.pathscontrol;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.projectfloodlight.openflow.types.DatapathId;
 
+import net.floodlightcontroller.core.types.NodePortTuple;
 import net.floodlightcontroller.linkdiscovery.Link;
 import net.floodlightcontroller.routing.Path;
 
@@ -11,15 +13,18 @@ public class CandidatePath extends Path{
 	
 	private long bandwidthConsumption;
 	private List<Link> links;
+	private List<DatapathId> switchesRefs;
 	
 	public CandidatePath(DatapathId src, DatapathId dst) {
 		super(src, dst);
+		initswitchesRefs();
 	}
 	
 	public CandidatePath(Path path) {
 		super(path.getId(), path.getPath());
 		setHopCount(path.getHopCount());
 		setLatency(path.getLatency());
+		initswitchesRefs();
 	}
 
 	public long getBandwidthConsumption() {
@@ -36,6 +41,19 @@ public class CandidatePath extends Path{
 
 	public void setLinks(List<Link> links) {
 		this.links = links;
+		
+	}
+	
+	private void initswitchesRefs(){
+		switchesRefs = new ArrayList<>();
+		for (NodePortTuple npt : getPath()) {
+			if(!switchesRefs.contains(npt))
+				switchesRefs.add(npt.getNodeId());
+		}
+	}
+	
+	public List<DatapathId> getSwitchesRefs(){
+		return switchesRefs;
 	}
 	
 	
--- floodlight/src/main/java/net/floodlightcontroller/classico/pathscontrol/ExecutorPathFlowSDN.java
+++ floodlight/src/main/java/net/floodlightcontroller/classico/pathscontrol/ExecutorPathFlowSDN.java
@@ -15,11 +15,15 @@ import org.projectfloodlight.openflow.types.EthType;
 import org.projectfloodlight.openflow.types.IPv4Address;
 import org.projectfloodlight.openflow.types.IpProtocol;
 import org.projectfloodlight.openflow.types.OFPort;
+import org.python.antlr.PythonParser.continue_stmt_return;
 
 import net.floodlightcontroller.classico.sessionmanager.Rule;
+import net.floodlightcontroller.classico.sessionmanager.Session;
+import net.floodlightcontroller.classico.sessionmanager.UserSession;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.internal.IOFSwitchService;
 import net.floodlightcontroller.core.types.NodePortTuple;
+import net.floodlightcontroller.linkdiscovery.Link;
 import net.floodlightcontroller.util.FlowModUtils;
 
 public class ExecutorPathFlowSDN {
@@ -88,7 +92,118 @@ public class ExecutorPathFlowSDN {
 				", Port: " + ofPort.getPortNumber() +", Reference: "+rule.getIpv4Src()+" -> "+rule.getIpv4Dst());
 	}
 	
+	
+	
+	public void  updateFlowPathsTest(List<MultipathSession> multipathSessions, HashMap<String, CandidatePath> bestPaths){
+		
+		HashMap<Session, HashMap<DatapathId, List<UserSession>>> branchsModel = new HashMap<>();
+		
+		for (Iterator<MultipathSession> iterator1 = multipathSessions.iterator(); iterator1.hasNext();) {
+			MultipathSession ms1 = (MultipathSession) iterator1.next();
+			Session session = ms1.getSessionMultiUser();
+			CandidatePath bp1 = bestPaths.get(ms1.getPathIndex());
+			
+			
+			for (Iterator<MultipathSession> iterator2 = multipathSessions.iterator(); iterator2.hasNext();) {
+				MultipathSession ms2 = (MultipathSession) iterator2.next();
+				
+				//Se ambos forem iguais vai para próxima iteração
+				if(ms1.equals(ms2)) continue;
+				CandidatePath bp2 = bestPaths.get(ms2.getPathIndex());
+				
+				//Se estão na mesma sessão
+				if(session.equals(ms2.getSessionMultiUser())){
+					
+					
+					HashMap<DatapathId, List<UserSession>> branch;
+
+					//Se ainda não existe a sessão no modelo
+					if(!branchsModel.containsKey(session)){
+						
+						branch = new HashMap<>();
+						
+					}else{
+						
+						branch = branchsModel.get(session);
+						
+					}
+					
+					//Pega o último nos dois grupos de caminhos
+					for (int i = bp2.getSwitchesRefs().size()-1; i >= 0; i--) {
+						
+						//Pega cada switch de trás pra frente do caminho-candidato-2
+						DatapathId dataPathId = bp2.getSwitchesRefs().get(i);
+						
+						//Se o switch tiver no caminho-candidato-1
+						if(bp1.getSwitchesRefs().contains(dataPathId)){
+							
+							//E ainda não tiver cadastrado como chave
+							if(!branchsModel.containsKey(session) || !branchsModel.get(session).containsKey(dataPathId)){
+								
+//								System.out.println(1);
+								
+								//Adiciona os dois usuários na lista
+								List<UserSession> users = new ArrayList<>();
+								users.add(ms1.getUserSession());
+								users.add(ms2.getUserSession());
+								//E adiciona em um branch, onde o switch apontará para os dois usuários
+								branch.put(dataPathId, users);
+								
+//								if(!branchsModel.containsKey(session)){
+								branchsModel.put(session, branch);
+//								}else{
+//									branchsModel.get(session).;
+//								}
+								
+								
+							}else if(branchsModel.get(session).containsKey(dataPathId)){
+//								System.out.println(2);
+								//Se ainda não tiver o usuário cadastrado
+								if(!branchsModel.get(session).get(dataPathId).contains(ms2.getUserSession())){
+									branchsModel.get(session).get(dataPathId).add(ms2.getUserSession());
+									
+								}		
+							}
+							
+							break;
+						}
+					}
+				}
+				
+				
+				
+				
+				//pegaUltimoSwitch em que pb1 e bp2 se interceptam
+				//o switch resultado é salvo num hashmap como chave para os dois
+				//se a chave já existe bp2 é adicionado a lista (caso ele não esteja) e adicionado a uma lista de não titular
+				
+				//38 -> h1 e h2
+				//32 -> h1 e h3 e h4
+				//14 -> h3 e h4
+				
+				
+//				HashMap<DatapathId, List<UserSession>> branchsModel;
+				
+				//cada sessão está associada a um mapeamento de (
+					//cada switche mapeia uma lista de usuários
+				
+				//)
+				//< Sessão-1, <Switch-s1 , 192.168.2.100, 192.168.2.110>>
+				//o ID da sessão determinará  o ID do grupo
+//				HashMap<Session, HashMap<DatapathId, List<UserSession>>> branchsModel;
+				// o numero de seesões determianrá o numero máximo de fluxos concorrentes
+				
+				//<Switch> <Cliente Titular, Lista de Demais>
+			}
+			
+		}
+//		System.out.println(branchsModel.toString());
+	}
+	
+	
 	public void  updateFlowPaths(List<MultipathSession> multipathSessions, HashMap<String, CandidatePath> bestPaths){
+		
+		updateFlowPathsTest(multipathSessions, bestPaths);
 		
 		try{
 			
@@ -109,6 +224,7 @@ public class ExecutorPathFlowSDN {
 			for (Iterator<MultipathSession> iterator = multipathSessions.iterator(); iterator.hasNext();) {
 				MultipathSession ms = (MultipathSession) iterator.next();
 				
+			
 				
 				if(keyEquals.contains(ms.getPathIndex())) continue;
 				
@@ -120,8 +236,7 @@ public class ExecutorPathFlowSDN {
 						
 						DatapathId dpi = npt.getNodeId();
 						OFPort ofp = npt.getPortId();
-//						System.out.print("[ExecutorPathFlowSDN] FLOW_MOD DELETE: Switch:" + dpi.toString() + 
-//								", Port:" + ofp.getPortNumber());
+
 						/*Reagra para pacotes do servidor para o cliente pelo IP*/
 						Rule matchIpFLow = new Rule(ms.getServerSession().getIp(), ms.getUserSession().getSrcIp().toString());
 						
@@ -136,16 +251,13 @@ public class ExecutorPathFlowSDN {
 				
 				/*Cria os novos*/
 				CandidatePath bp = bestPaths.get(ms.getPathIndex());
-//				System.out.println(bestPaths.size());
+
 				if(bp != null){
 					
 					for (int i = 0; i < bp.getPath().size(); i+=2) {
 						NodePortTuple npt = bp.getPath().get(i);
 						DatapathId dpi = npt.getNodeId();
 						OFPort ofp = npt.getPortId();
-						
-//						System.out.println("[ExecutorPathFlowSDN] FLOW_MOD ADD: Switch:" + dpi.toString() + 
-//								", Port:" + ofp.getPortNumber());
 						
 						/*Reagra para pacotes do servidor para o cliente pelo IP*/
 						Rule matchIpFLow = new Rule(ms.getServerSession().getIp(), ms.getUserSession().getSrcIp().toString());
