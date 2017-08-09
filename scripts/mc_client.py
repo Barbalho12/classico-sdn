6e2870aa72a31434f437c254ebb10f8b19dedb41
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-08-09 11:36:45 -0300

    Evalvid + multi-cast

--- scripts/mc_client.py
+++ scripts/mc_client.py
@@ -1,6 +1,8 @@
 import socket
 import struct
 import sys
+import subprocess
+import os
 
 multicast_group = '224.3.29.71'
 server_address = ('', 10000)
@@ -20,12 +22,24 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 
 # Receive/respond loop
-while True:
-    print >>sys.stderr, '\nwaiting to receive message'
-    data, address = sock.recvfrom(1024)
-    
-    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >>sys.stderr, data
+# while True:
 
-    print >>sys.stderr, 'sending acknowledgement to', address
-    sock.sendto('ack', address)
+
+    # print >>sys.stderr, '\nwaiting to receive message'
+script = "cd ../evalvid &&"
+script += "rm -rf files/sd"+sys.argv[1]+" &&"
+script += "ufw enable &&"
+script += "tcpdump -n -tt -v udp port 10000 >  files/sd"+sys.argv[1]
+os.system(script)
+# os.system("cd ../evalvid &&")
+# os.system("rm -rf files/sd"+sys.argv[1]+"")
+# os.system("ufw enable"+"")
+# os.system("tcpdump -n -tt -v udp port 10000 >  files/sd"+sys.argv[1])
+    # os.system("cd ../evalvid && ./mp4trace -f -s 224.3.29.71 10000 sample.mp4 > files/st01")
+    # data, address = sock.recvfrom(1024)
+    
+    # print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    # print >>sys.stderr, data
+
+    # print >>sys.stderr, 'sending acknowledgement to', address
+    # sock.sendto('ack', address)
--- scripts/mc_server.py
+++ scripts/mc_server.py
@@ -1,6 +1,9 @@
 import socket
 import struct
 import sys
+import subprocess
+import shlex
+import os
 
 message = 'very important data'
 multicast_group = ('224.3.29.71', 10000)
@@ -24,17 +27,20 @@ try:
     # Send data to the multicast group
     print >>sys.stderr, 'sending "%s"' % message
     sent = sock.sendto(message, multicast_group)
+    # subprocess.call(['cd ../evalvid && ./mp4trace -f -s 224.3.29.71 10000 sample.mp4 > files/st01'])
+    # subprocess.call(shlex.split('cd ../evalvid && ls'))
+    os.system("cd ../evalvid && ./mp4trace -f -s 224.3.29.71 10000 sample.mp4 > files/st01")
 
     # Look for responses from all recipients
-    while True:
-        print >>sys.stderr, 'waiting to receive'
-        try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            print >>sys.stderr, 'timed out, no more responses'
-            break
-        else:
-            print >>sys.stderr, 'received "%s" from %s' % (data, server)
+    # while True:
+    #     print >>sys.stderr, 'waiting to receive'
+    #     try:
+    #         data, server = sock.recvfrom(16)
+    #     except socket.timeout:
+    #         print >>sys.stderr, 'timed out, no more responses'
+    #         break
+    #     else:
+    #         print >>sys.stderr, 'received "%s" from %s' % (data, server)
 
 finally:
     print >>sys.stderr, 'closing socket'
