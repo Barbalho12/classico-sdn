649f9f864bd3dfd4d5792236d519b041e38f0ef4
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-08-14 23:29:08 -0300

    Update in tests CLASSICO scripts

--- scripts/classico_server.py
+++ scripts/classico_server.py
@@ -1,65 +1,47 @@
 import socket
 import struct
 import sys
-# import subprocess
-# import shlex
 import os
-# import threading
 import thread
 import time
 
 
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 10000 # Arbitrary non-privileged port
- 
+portAddr = 10000
+ipAddr = ''
 # Datagram (udp) socket
 try :
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print 'Socket created'
-except socket.error, msg :
-    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
- 
- 
-# Bind socket to local host and port
-try:
     s.bind((HOST, PORT))
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
-print 'Socket bind complete'
-
-
-try:
+    print 'Socket bind complete'
     newRequest = s.recvfrom(1024)
     nameFile = newRequest[0] #nome do arquivo video
     addr = newRequest[1] #Endereco do cliente
     ipAddr = addr[0]
     portAddr = addr[1]
+# except socket.error, msg :
+#     print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+#     sys.exit()
+finally:
+    print >> sys.stderr, 'closing socket'
     s.close()
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
-
-
+ 
 
 def startClient():
     os.system("cd ../evalvid && ./client.sh")
-    # time.sleep(1)
-    
+    # tcpdump -i any -n -tt -v udp port 10000 -c 8666 > files/rd01 
+    sys.exit()
 
-# try:
+    
 raw_input("Press Enter to continue...")
 thread.start_new_thread(startClient, ())
 time.sleep(1)
 os.system("cd ../evalvid && ./mp4trace -f -s "+ipAddr+" 10000 sample.mp4 > files/st01")
 time.sleep(1)
 os.system("sudo kill -2 $(ps -C 'tcpdump' -o pid=)")
+# os.system("sudo kill -2 $(ps -C 'tcpdump -i any -n -tt -v udp port 10000' -o pid=)")
 
-# finally:
-    # print >> sys.stderr, 'closing socket'
-    # sock.close()
+
 
