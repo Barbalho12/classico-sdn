fd67d1cdb3b781d460236522bd827ab6116436c8
Felipe Brabalho <felipebarbalho.95@gmail.com>
2017-11-05 00:41:53 -0300

    Edit mcast protocol

--- scripts/mcast_v1/mc_client.py
+++ scripts/mcast_v1/mc_client.py
@@ -5,9 +5,9 @@ import subprocess
 import os
 import time
 
-multicast_group = '224.3.29.72'
-os.system("route add -host 224.3.29.72 "+sys.argv[1]+"-eth0")
-server_address = ('', 10000)
+# multicast_group = '224.3.29.72'
+# os.system("route add -host 224.3.29.72 "+sys.argv[1]+"-eth0")
+# server_address = ('', 10000)
 
 ip_server = '192.168.2.1'
 port_server = 10000
@@ -16,31 +16,72 @@ content = "sample.mp4"
 # Script for evaluation of evalvid
 script_evalvid = "cd ../../evalvid &&"
 script_evalvid += "rm -rf files/sd"+sys.argv[1]+" &&"
-script_evalvid += "ufw enable  > /dev/null &&"
+# script_evalvid += "ufw enable  > /dev/null &&"
 script_evalvid += "tcpdump -n -tt -v udp port 10000 >  files/sd"+sys.argv[1]
 
+# # Takes the request time 
+# os.system("cd ../../evalvid && tcpdump ether multicast -n -tt -c 1 > files/ts"+sys.argv[1]+" &") 
+
 # Takes the request time 
-os.system("cd ../../evalvid && tcpdump ether multicast -n -tt -c 1 > files/ts"+sys.argv[1]+" &") 
+os.system("cd ../../evalvid && tcpdump -i any -n -tt -v udp -c 1 > files/ts"+sys.argv[1]+" &") 
+
 
 # Do not interfere in anything
 time.sleep(2)
 
 try:
-	# Create the socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# Create socket send by 10000 port 
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('', 10001))
 
-	# Bind to the server address
-	sock.bind(server_address)
+    # Show init time (no setup time)
+	os.system("echo CLIENT "+sys.argv[1]+" INIT $(date +'%F %T,%3N') ")
+	
+	print("Send video request..")
+	s.sendto(content, (ip_server, 10001))
+
+	print("Waiting group address..")
+	receive = s.recvfrom(1024)
+
+	group_addr = receive[0]
+
+	# multicast_group = '224.3.29.72'
+	os.system("route add -host "+group_addr+" "+sys.argv[1]+"-eth0")
 
 	# Tell the operating system to add the socket to the multicast group
 	# on all interfaces.
-	group = socket.inet_aton(multicast_group)
+	# group = socket.inet_aton(multicast_group)
+	group = socket.inet_aton(group_addr)
 	mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-finally:
-    # print >> sys.stderr, 'closing socket'
-    sock.close()
-    # sys.exit()
+	s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	#Send ok to server
+	s.sendto(group_addr, (ip_server, 10002))
+
+	#Run script for evaluation of evalvid
+	# os.system(script_evalvid)
+
+	s.close()
+
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+
+# try:
+	# # Create the socket
+	# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	# # Bind to the server address
+	# sock.bind(server_address)
+
+	# # Tell the operating system to add the socket to the multicast group
+	# # on all interfaces.
+	# group = socket.inet_aton(multicast_group)
+	# mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+	# sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+# finally:
+    # sock.close()
+
 
 # Close socket
 # s.close()
--- scripts/mcast_v1/mc_server.py
+++ scripts/mcast_v1/mc_server.py
@@ -4,6 +4,7 @@ import sys
 import os
 import thread
 import time
+import threading
 
 multicast_group = ('224.3.29.71', 10000)
 os.system("route add -host 224.3.29.71 "+sys.argv[1]+"-eth0")
@@ -20,18 +21,74 @@ sock.settimeout(0.2)
 ttl = struct.pack('b', 1)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-try:
 
-    os.system("cd ../../evalvid && ./client.sh &")
-    os.system("cd ../../evalvid && ./mp4trace -f -s 224.3.29.71 10000 sample.mp4 > files/st01")
-    
-    os.system("echo SERVER FINISH $(date +'%F %T,%3N') ")
+# HOST = ''   # Symbolic name meaning all available interfaces
+# PORT = 10000 # Arbitrary non-privileged port
+# portAddr = 10000
+# ipAddr = ''
 
-    time.sleep(1)
+# Python create mutex  
+my_mutex = threading.Lock()  
 
-    os.system("sudo kill -2 $(ps -C 'tcpdump' -o pid=)")
+s_controler = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s_controler.bind(('', 10002))
 
-finally:
-    print >> sys.stderr, 'closing socket'
-    sock.close()
+def send_client(ipAddr, portAddr, nameFile):
+	try :
+		
+		ip_group = "224.3.29.72"
 
+		# Send group address to client
+		s_controler.sendto(ip_group, (ipAddr, 10001))
+
+		okRequest = s_controler.recvfrom(1024)
+		print("Client "+str(ipAddr)+":"+str(portAddr)+" > "+nameFile+" OK")
+		
+		# s_controler.close()
+		my_mutex.release()  # global my_mutex
+		print ("++++ Release")
+	except :
+		print "Ops: send_client ERRO"
+		# sys.exit()
+
+
+def request_client():
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('', 10001))
+
+	try :
+
+		while(True):
+
+			# Receive client request
+			newRequest = s.recvfrom(20)
+			
+			nameFile = newRequest[0] #nome do arquivo video
+			addr = newRequest[1] #Endereco do cliente
+			ipAddr = addr[0]
+			portAddr = addr[1]
+
+			# print("New Client "+str(ipAddr)+":"+str(portAddr)+" > "+nameFile)
+			thread.start_new_thread( send_client, (ipAddr, portAddr, nameFile) )
+
+	except socket.error, msg :
+		print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
+
+	finally:
+		s.close()
+
+# Mutex for concurrent client requests
+my_mutex.acquire()
+t = thread.start_new_thread( request_client, () )
+
+# After the first request is processed the mutex is released and the content can be sent
+my_mutex.acquire()
+
+# Evalvid evaluation
+os.system("cd ../../evalvid && ./client.sh &")
+os.system("cd ../../evalvid && ./mp4trace -f -s 224.3.29.71 10000 sample.mp4 > files/st01")
+os.system("echo SERVER FINISH $(date +'%F %T,%3N') ")
+time.sleep(1)
+os.system("sudo kill -2 $(ps -C 'tcpdump' -o pid=)")
